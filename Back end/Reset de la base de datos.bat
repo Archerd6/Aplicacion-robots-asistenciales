c1667fd592250ced5db1d29312982b1a24fac6c9
Archerd6 <archerd@uma.es>
2024-04-26 13:03:38 +0200

    Limpieza de código 🧹

--- Back end/Reset de la base de datos.bat
+++ Back end/Reset de la base de datos.bat
@@ -8,13 +8,6 @@ cd ..
 
 python3 Creacion_de_la_base_de_datos.py %*
 
-REM Ejecutable para iniciar el servidor en windows
-cd .env
-cd Scripts
-call activate.bat
-cd ..
-cd ..
-
 start "" "Reset de la base de datos.html"
 
 cls
